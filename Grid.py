a15578cbf97da6adb672ce99a2aa1d2a0ef45a9d
Thomas <tvandev4@uwo.ca>
2019-08-06 03:53:09 -0400

    added to Grid.py and working on graphic drawing script for docs

--- Grid.py
+++ Grid.py
@@ -12,23 +12,35 @@ def flat_to_matrix3d(l: int, m: int, i: int) -> Triple:
     y, x = divmod(l, y)
     return (x, y, z)
 
-def matrix2d_to_flat(n : int, i : int, j : int) -> int:
-    return i * n + j 
+def matrix2d_to_flat(m : int, i : int, j : int) -> int:
+    return i * m + j
 
 def matrix2d_to_diagonal(m : int, i : int, j : int) -> Double:
+    return  (i+j, j if i + j < m else j + m - (i+j) )
 
-    return (i*n)
+def diagonal_to_matrix2d(m : int, a : int, b: int) -> Double:
+    return (
+        a - b if a < m else m - b, 
+        b     if a < m else a + b - m )
 
 def flat_to_diagonal(n : int, i : int) -> Double:
     '''
     flat_to_diagonal
     ================
-    Sequences the squares on the grid using diagonal coordinates. 
+    Sequences the squares in diagonal coordinates on a square matrix. 
+    
+    Parameters
+    ----------
+    n : int
+        the size of the square matrix
+    i : int
+        the index in the flat array
 
     Returns
-        Tuple[int,int]
+    -------
+    Double
     '''
-    return matrix2d_to_diagonal()
+    return matrix2d_to_diagonal(n, i // n, i % n)
 
 flip_matrix2d_horizontal()
 
--- make_graphics.py
+++ make_graphics.py
@@ -1,8 +1,20 @@
-import pillow
+from PIL import Image, ImageDraw
 import typing
 import typing_extensions
 import Grid
 
-def make_graphic(n : int, m : int, it : typing.Iterable) -> None:
+def make_graphic(n: int, it : typing.Iterable[int], name : str) -> None:
     
-    for 
+    size = 512
+    im = Image.new('RGB', (size, size))
+    squareSize = size / n
+    colorFraction = 256 / (n*n)
+    draw = ImageDraw.Draw(im)
+    for i in it:
+        x, y = Grid.flat_to_matrix2d(n, i)
+        draw.rectangle( 
+            (x*squareSize,y*squareSize, (x+1)*squareSize, (y+1)*squareSize), 
+            fill= i*colorFraction )
+        
+    im.save(name, 'PNG')
+    
