8724b0230045f2cedbdc2a797b325326476afc1f
Thomas <tvandev4@uwo.ca>
2019-08-06 18:27:41 -0400

    updated Grid.py

--- Grid.py
+++ Grid.py
@@ -21,7 +21,7 @@ def flat_to_matrix2d(m: int, i : int) -> Tuple[int, int]:
     Parameters
     ----------
     m : int
-        the row size of the matrix. 
+        The row size of the matrix. 
         We do not need the columnsize because we do not check bounds.
     i : int
         the index in the array to convert
@@ -46,7 +46,8 @@ def flat_to_diagonal(n : int, i : int) -> Tuple[int,int]:
     Parameters
     ----------
     n : int
-        the size of the square matrix
+        The row and column size.
+        Diagonal coordinates are only defined for square matrices.
     i : int
         the index in the flat array
 
@@ -58,18 +59,80 @@ def flat_to_diagonal(n : int, i : int) -> Tuple[int,int]:
     return if int(sqrt(2*i+0.25) -0.5 )
 
 def matrix2d_to_flat(m : int, i : int, j : int) -> int:
+    '''
+    matrix2d_to_flat
+    ================
+    Convert matrix2d coordinates into 1d flat coordinates.
+
+    Parameters
+    ----------
+    m : int
+        the row size of the matrix
+    i : int
+        row index
+    j : int
+        column index
+    
+    Returns
+    -------
+    int
+        the flat coordinate
+    '''
     return i * m + j
 
-def matrix2d_to_diagonal(n : int, i : int, j : int) -> Tuple[int,int]:
-    return  (i+j, j if i + j < n else n - i )
+def matrix2d_to_diagonal(n : int, i : int, j : int) -> Tuple[int, int]:
+    '''
+    matrix2d_to_diagonal
+    ====================
+    Convert matrix coordinates to diagonal coordinates.
 
-def diagonal_to_matrix2d(n : int, a : int, b: int) -> Tuple[int,int]:
+    Parameters
+    ----------
+    n : int
+        The row and column size for a square matrix.
+        Diagonal coordinates are only defined for square matrices.
+    i : int
+        the row index.
+    j : int
+        the column index.
+
+    Returns
+    -------
+    Tuple[int, int]
+        The resultant diagonal coordinate.
+    '''
+    return  (
+        i+j,
+        j if i + j < n else n - i )
+
+def diagonal_to_matrix2d(n : int, a : int, b: int) -> Tuple[int, int]:
+    '''
+    diagonal_to_matrix2d
+    ====================
+    Convert diagonal coordinate to the corresponding
+    position in matrix coordinates.
+
+    Parameters
+    ----------
+    n : int
+        The row and column size for a square matrix.
+        Diagonal coordinates are only defined for square matrices.
+    a : int
+        The index of the diagonal.
+    b : int
+        The index on the diagonal a.
+
+    Returns
+    -------
+    Tuple[int, int]
+        The resulting matrix coordinate.
+    '''
     return (
         a - b if a < n else n - b, 
         b     if a < n else a + b - n )
 
 
-#ToDO : Flip and rotate methods
+#ToDO : Flip rotate and transpose. 
 
 if __name__ == '__main__':
     def matrix_to_string(n : int):
