9dc216892803a6e9beb323585074ea3c15e95937
Thomas <tvandev4@uwo.ca>
2019-08-06 04:30:26 -0400

    more changes to make_graphics

--- Grid.py
+++ Grid.py
@@ -4,26 +4,40 @@ import functools
 Double = Tuple[int, int]
 Triple = Tuple[int, int, int]
 
-def flat_to_matrix2d(m: int, i : int) -> Double:
+'''
+Grid
+====
+This module provides functions to do convert between
+labellings of the
+'''
+
+
+def flat_to_matrix2d(m: int, i : int) -> Tuple[int, int]:
+    '''
+    flat_to_matrix2d
+    ================
+    Convert 1d coordinates to 2d matrix coordinates.
+
+    Parameters
+    ----------
+    m : int
+        the row size of the matrix. 
+        We do not need the columnsize because we do not check bounds.
+    i : int
+        the index in the array to convert
+    
+    Returns
+    -------
+    Tuple[int, int]
+    '''
     return divmod(m, i)
 
-def flat_to_matrix3d(l: int, m: int, i: int) -> Triple:
+def flat_to_matrix3d(l: int, m: int, i: int) -> Tuple[int,int,int]:
     z, y = divmod(l*m, i)
     y, x = divmod(l, y)
     return (x, y, z)
 
-def matrix2d_to_flat(m : int, i : int, j : int) -> int:
-    return i * m + j
-
-def matrix2d_to_diagonal(m : int, i : int, j : int) -> Double:
-    return  (i+j, j if i + j < m else j + m - (i+j) )
-
-def diagonal_to_matrix2d(m : int, a : int, b: int) -> Double:
-    return (
-        a - b if a < m else m - b, 
-        b     if a < m else a + b - m )
-
-def flat_to_diagonal(n : int, i : int) -> Double:
+def flat_to_diagonal(n : int, i : int) -> Tuple[int,int]:
     '''
     flat_to_diagonal
     ================
@@ -42,10 +56,23 @@ def flat_to_diagonal(n : int, i : int) -> Double:
     '''
     return matrix2d_to_diagonal(n, i // n, i % n)
 
-flip_matrix2d_horizontal()
+def matrix2d_to_flat(m : int, i : int, j : int) -> int:
+    return i * m + j
+
+def matrix2d_to_diagonal(m : int, i : int, j : int) -> Tuple[int,int]:
+    return  (i+j, j if i + j < m else j + m - (i+j) )
+
+def diagonal_to_matrix2d(m : int, a : int, b: int) -> Tuple[int,int]:
+    return (
+        a - b if a < m else m - b, 
+        b     if a < m else a + b - m )
+
+
+#ToDO : Flip and rotate methods
 
 if __name__ == '__main__':
     def matrix_to_string(n : int):
-        for i in 
+        for i in range(10):
+            pass
     
 
--- make_graphics.py
+++ make_graphics.py
@@ -1,4 +1,5 @@
 from PIL import Image, ImageDraw
+from functools import partial
 import typing
 import typing_extensions
 import Grid
@@ -8,13 +9,17 @@ def make_graphic(n: int, it : typing.Iterable[int], name : str) -> None:
     size = 512
     im = Image.new('RGB', (size, size))
     squareSize = size / n
-    colorFraction = 256 / (n*n)
+    colorFraction = 255 / (n*n)
+
     draw = ImageDraw.Draw(im)
     for i in it:
         x, y = Grid.flat_to_matrix2d(n, i)
         draw.rectangle( 
             (x*squareSize,y*squareSize, (x+1)*squareSize, (y+1)*squareSize), 
-            fill= i*colorFraction )
+            fill = i*colorFraction )
         
     im.save(name, 'PNG')
-    
+
+if __name__ == '__main__':
+    n = 8
+    make_graphic(n, range(n,n), 'regular')
