997db1b4e5a2b262700529e98a9aeb7b4a093f65
Thomas <tvandev4@uwo.ca>
2019-08-06 17:24:01 -0400

    changes to Grid.py

--- Grid.py
+++ Grid.py
@@ -30,11 +30,11 @@ def flat_to_matrix2d(m: int, i : int) -> Tuple[int, int]:
     -------
     Tuple[int, int]
     '''
-    return divmod(m, i)
+    return divmod(i, m)
 
 def flat_to_matrix3d(l: int, m: int, i: int) -> Tuple[int,int,int]:
-    z, y = divmod(l*m, i)
-    y, x = divmod(l, y)
+    z, y = divmod(i, l*m)
+    y, x = divmod(i, l)
     return (x, y, z)
 
 def flat_to_diagonal(n : int, i : int) -> Tuple[int,int]:
@@ -54,18 +54,19 @@ def flat_to_diagonal(n : int, i : int) -> Tuple[int,int]:
     -------
     Double
     '''
-    return matrix2d_to_diagonal(n, i // n, i % n)
+    a_series = lambda x, y : (x + y) * (x - y + 1) // 2
+    return if int(sqrt(2*i+0.25) -0.5 )
 
 def matrix2d_to_flat(m : int, i : int, j : int) -> int:
     return i * m + j
 
-def matrix2d_to_diagonal(m : int, i : int, j : int) -> Tuple[int,int]:
-    return  (i+j, j if i + j < m else j + m - (i+j) )
+def matrix2d_to_diagonal(n : int, i : int, j : int) -> Tuple[int,int]:
+    return  (i+j, j if i + j < n else n - i )
 
-def diagonal_to_matrix2d(m : int, a : int, b: int) -> Tuple[int,int]:
+def diagonal_to_matrix2d(n : int, a : int, b: int) -> Tuple[int,int]:
     return (
-        a - b if a < m else m - b, 
-        b     if a < m else a + b - m )
+        a - b if a < n else n - b, 
+        b     if a < n else a + b - n )
 
 
 #ToDO : Flip and rotate methods
--- make_graphics.py
+++ make_graphics.py
@@ -1,5 +1,7 @@
 from PIL import Image, ImageDraw
 from functools import partial
+import sys
+import os
 import typing
 import typing_extensions
 import Grid
@@ -7,19 +9,38 @@ import Grid
 def make_graphic(n: int, it : typing.Iterable[int], name : str) -> None:
     
     size = 512
-    im = Image.new('RGB', (size, size))
-    squareSize = size / n
-    colorFraction = 255 / (n*n)
 
+    im = Image.new('RGB', (size, size), (0,255,255))
     draw = ImageDraw.Draw(im)
+
+    squareSize = size / n
+
+    def color(i : int):
+        colors = [
+            (255, 0, 0), 
+            #(255, 255, 0),
+            #(0,255, 0),
+            #(0, 255, 255),
+            #(0, 0, 255),
+            (0, 0, 255)
+        ]
+        j = int( (len(colors)-1)* i / (n*n) )
+        t = (len(colors) -1)* i / (n*n) - j
+        j = int(j)
+        return tuple( int( (1-t)*a + t*b ) for a,b in zip(colors[j], colors[j+1]) )
+
+
     for i in it:
         x, y = Grid.flat_to_matrix2d(n, i)
         draw.rectangle( 
             (x*squareSize,y*squareSize, (x+1)*squareSize, (y+1)*squareSize), 
-            fill = i*colorFraction )
+            fill = color(i) )
         
-    im.save(name, 'PNG')
+    im.save( name + '.png', 'PNG' )
 
 if __name__ == '__main__':
     n = 8
-    make_graphic(n, range(n,n), 'regular')
+    flat_to_flat_diagonal = lambda i : Grid.matrix2d_to_flat(n,
+            *Grid.diagonal_to_matrix2d(n, 
+                *Grid.flat_to_diagonal(n, i)))
+    make_graphic(n, map(flat_to_flat_diagonal, range(n*n)), 'regular' )
