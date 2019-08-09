49e85c8fa422600ebe285ef1f28726548e1dcfdd
Thomas <tvandev4@uwo.ca>
2019-08-09 04:05:08 -0400

    updated graphics

--- make_graphics.py
+++ make_graphics.py
@@ -1,15 +1,15 @@
 from PIL import Image, ImageDraw, ImageFont
-from typing import Iterable, Tuple, Callable
+from typing import Iterable, Tuple, Callable, List
+import math
 import typing_extensions
 import Grid
 
 def matrix_graphic(
-    im : Image
+    im : Image,
     n: int, 
-    coordinate : Callable[int, Tuple[int,int]],
-    name : str,
-    size : int = 512,
-    colors : List[Tuple[int,int,int]] = [(255,0,0),(0,255,0)]) -> None:
+    coordinate : Callable[[int], Tuple[int,int]],
+    size : int,
+    colors : List[Tuple[int,int,int]] = [(255,0,0),(0,255,0)] ) -> None:
     '''
     matrix_graphic
     ==============
@@ -25,9 +25,6 @@ def matrix_graphic(
         Get the matrix coordinate of the current flat index.
         You could for example
     
-    name : str
-        the name for the resulting file. .png extension added automatically
-    
     size : int = 512
         the length and width of the image. defaults to 512.
     
@@ -40,40 +37,41 @@ def matrix_graphic(
     None
         generates a file with filename name + '.png' in the current directory.
     '''
-    
     draw = ImageDraw.Draw(im)
 
-    squareSize = size / n
-
-    def color(colors : Tuple[int,int,int], gradient : float):
+    def color(gradient : float) -> Tuple[int,int,int]:
         
-        if len(colors) <= 1:
-            return (0,0,0)
-
-        i = math.ceil( (len(colors) - 2) * gradient - 1 )
+        i = math.floor( (len(colors) - 1) * gradient )
         t = ( len(colors) - 1 ) * gradient - i
         
-        c1, c2 = colors[i:i+2]
+        combine_colors = lambda c1, c2 : tuple( 
+            int( (1-t)*a + t*b ) for a,b in zip(c1, c2) )
 
-        return ( int( (1-t)*a + t*b ) for a,b in zip(c1, c2) )
-
+        return combine_colors(*colors[i:i+2])
 
     for i in range(n*n):
         
         y, x = coordinate(i)
-
+        square = size/n
         draw.rectangle( 
-            (x*size/n, y*size/n, (x+1)*size/n, (y+1)*size/n), 
-            fill = color( i / (n*n) )
-        fnt = ImageFont.truetype("FreeMonoBold.ttf", int(squareSize*0.75))
+            (x*square, y*square, (x+1)*square, (y+1)*square), 
+            fill = color( i / (n*n) ) 
+        )
 
-        draw.text((x*squareSize,y*squareSize), str(i), font=fnt, fill=(0,0,0))
+        fnt = ImageFont.truetype("NotoMono-Regular", int(square*0.6))
+        def center_text(x, y, text):
+            width, height = fnt.getsize(text)
+            return (x - width/2, y - height/2)
+        
+        draw.text( 
+            center_text( (x+0.5)*square, (y+0.5)*square, str(i)), 
+            str(i), font=fnt, fill=(0,0,0))
     
     # make the lines
     for i in range(n):
 
-        draw.line( (0,i*squareSize, size, i*squareSize), fill=(0,0,0))
-        draw.line( (i*squareSize, 0, i*squareSize, size), fill=(0,0,0))
+        draw.line( (0,i*square, size, i*square), fill=(0,0,0))
+        draw.line( (i*square, 0, i*square, size), fill=(0,0,0))
 
     draw.line( (0, size-1, size-1, size-1), fill=(0,0,0))
     draw.line( (size-1, 0, size-1, size-1), fill=(0,0,0))
@@ -84,15 +82,17 @@ if __name__ == '__main__':
     import sys
 
     n = int(sys.argv[1])
-
+    size = int(sys.argv[2])
     diagonal = lambda i : Grid.diagonal_to_matrix2d(n, *Grid.flat_to_diagonal(n, i))
     matrix2d = lambda i : Grid.flat_to_matrix2d(n, i)
 
     im1 = Image.new('RGB', (size, size), (0,0,0))
     im2 = Image.new('RGB', (size, size), (0,0,0))
     
-    matrix_graphic(im, n, matrix2d, 'matrix' )
-    matrix_graphic(im2, n, diagonal)
+    colors = [(255,0,0), (0,255,255)]
+
+    matrix_graphic(im1, n, matrix2d, size, colors)
+    matrix_graphic(im2, n, diagonal, size, colors)
 
     im1.save( 'matrix.png', 'PNG' )
     im2.save( 'diagonal.png', 'PNG' )
